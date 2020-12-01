0cf1cc1a56c9f4e5f0fac7da119fb2ac800a9a87
cereskou <cereskou@gmail.com>
2020-12-01 22:37:44 +0900

    Create roles.sql

---
+++ sql/roles.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `roles` (
+  `id` varchar(64) NOT NULL COMMENT 'Id',
+  `name` varchar(100) NOT NULL COMMENT 'ロール名',
+  `update_user` varchar(100) NOT NULL COMMENT '更新者',
+  `update_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
