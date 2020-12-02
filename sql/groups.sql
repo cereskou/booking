46470923a27de8eec669128dc3a71330c099f30b
cereskou <cereskou@gmail.com>
2020-12-02 17:39:07 +0900

    Update groups.sql

--- sql/groups.sql
+++ sql/groups.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `groups` (
-  `id` varchar(64) NOT NULL COMMENT 'グループID',
+  `id` bigint(20) unsigned NOT NULL COMMENT 'グループID',
   `name` varchar(256) NOT NULL COMMENT 'ユーザー名',
-  `update_user` varchar(64) NOT NULL COMMENT '更新者',
-  `update_date` datetime NOT NULL COMMENT '更新日時'
+  `update_user` bigint(20) unsigned NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='グループ'
