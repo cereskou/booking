4a74eb1bc03310245e445c2e35f558bbfd0e391e
cereskou <cereskou@gmail.com>
2020-12-02 17:34:36 +0900

    Update roles.sql

--- sql/roles.sql
+++ sql/roles.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `roles` (
-  `id` varchar(64) NOT NULL COMMENT 'Id',
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Id',
   `name` varchar(100) NOT NULL COMMENT 'ロール名',
-  `update_user` varchar(64) NOT NULL COMMENT '更新者',
-  `update_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
+  `update_user` bigint(20) unsigned NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '更新日時',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ロール'
